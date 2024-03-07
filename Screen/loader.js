e41ffb7ab3aa3405074c3d4a6b8b48a9d5027504
raaj21 <raaj92.m@gmail.com>
2024-03-07 15:21:34 +0530

    second commit

---
+++ Screen/loader.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
+export const setLoading = () => {
+  return (
+    <View style={[loader.container, loader.horizontal]}>
+      <ActivityIndicator />
+      <ActivityIndicator size="large" />
+      <ActivityIndicator size="small" color="#0000ff" />
+      <ActivityIndicator size="large" color="#00ff00" />
+    </View>
+  );
+};
+
+const loader = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10,
+  },
+});
